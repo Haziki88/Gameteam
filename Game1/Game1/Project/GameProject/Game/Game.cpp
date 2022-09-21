25620800078a2855b5f6459283a2182df886021e
otowq <happa0503@docomo.ne.jp>
2022-09-21 11:48:51 +0900

    a

--- Game1/Game1/Project/GameProject/Game/Game.cpp
+++ Game1/Game1/Project/GameProject/Game/Game.cpp
@@ -2,7 +2,6 @@
 #include "Field.h"
 #include "Player.h"
 #include "Enemy.h"
-#include "Goal.h"
 #include "../Title/Title.h"
 
 Game::Game() :Base(eType_Scene)
@@ -12,7 +11,7 @@ Game::Game() :Base(eType_Scene)
 	Base::Add(new Enemy(CVector2D(1280 + 256 * 1, 540), true));
 	Base::Add(new Enemy(CVector2D(1280 + 256 * 2, 540), true));
 	Base::Add(new Enemy(CVector2D(1280 + 256 * 3, 540), true));
-	Base::Add(new Goal(CVector2D(2048, 540)));
+	
 
 
 }
@@ -28,9 +27,9 @@ Game::~Game()
 void Game::Update()
 {
 	//ゴールが無ければゲームシーン終了
-	if (!Base::FindObject(eType_Goal)) {
-		SetKill();
-	}
+	//if (!Base::FindObject(eType_Goal)) {
+	//	SetKill();
+	//}
 
 	//プレイヤー死亡　ボタン１でゲームシーン終了
 	if (!Base::FindObject(eType_Player) && PUSH(CInput::eButton1)) {
