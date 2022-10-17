c9f9712282b0d3b2be8e769feb19f800bac16c8a
otowq <happa0503@docomo.ne.jp>
2022-10-17 10:38:14 +0900

    aa

--- Game1/Game1/Project/GameProject/Game/Game.cpp
+++ Game1/Game1/Project/GameProject/Game/Game.cpp
@@ -2,17 +2,13 @@
 #include "Field.h"
 #include "Player.h"
 #include "Enemy.h"
-#include "bird.h"
-#include "../Title/Title.h"
+//#include "../Title/Title.h"
 #include"../Game over/Game over.h"
 
 Game::Game() :Base(eType_Scene)
 {
-	Base::Add(new Field());
-	Base::Add(new Player(CVector2D(100,540), false));
-	Base::Add(new Enemy(CVector2D(700, 500), false));
-	Base::Add(new bird(CVector2D(600, 500), false));
-	SOUND("BGM_maoudamasii")->Play(true);
+	//Base::Add(new Field());
+	//SOUND("BGM_maoudamasii")->Play(true);
 
 }
 
@@ -21,7 +17,7 @@ Game::~Game()
 	//全てのオブジェクトを破棄
 	Base::KillAll();
 	//タイトルシーンへ
-	Base::Add(new Title());
+	//Base::Add(new Title());
 
 	if (!Base::FindObject(eType_Player) ) {
 		Base::Add(new Gameover());
